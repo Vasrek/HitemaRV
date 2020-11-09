d1227a8b55eb01e3222a27db616e6dc8a4f31f01
Theo <theo.denanassy@epitech.eu>
2020-11-09 17:02:18 +0100

    HealthCollectible

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth) // SI RUBY EST FULL HP ELLE NE PEUT SE SOIGNER
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,17 +7,24 @@ using Debug = UnityEngine.Debug;
 
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    public float speed = 3.0f;
 
+    public int health { get { return currentHealth; } }
+
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = 2;
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
     // Update is called once per frame
@@ -30,9 +37,16 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
